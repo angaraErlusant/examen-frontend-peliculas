feac976e61664b2731dd388258f6af4ae20a70ae
angaraErlusant <angara06.24q@gmail.com>
2026-03-13 09:44:21 -0600

    Update crear.js
    actualización url

--- js/crear.js
+++ js/crear.js
@@ -1,4 +1,4 @@
-const url = 'https://tu-api.onrender.com/peliculas';
+const url = 'https://examen-backend-peliculas.onrender.com';
 
 document.getElementById('formulario').addEventListener('submit', async (e) => {
     e.preventDefault();
