69f5b5775944922d2bc833c7e3550ce68adcc803
angaraErlusant <angara06.24q@gmail.com>
2026-03-13 09:45:01 -0600

    Update eliminar.js

--- js/eliminar.js
+++ js/eliminar.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
-const url = `https://tu-api.onrender.com/peliculas/${id}`;
+const url = `https://examen-backend-peliculas.onrender.com${id}`;
 
 document.getElementById('btnEliminar').addEventListener('click', async () => {
     await fetch(url, {
